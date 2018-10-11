d5e516ab5bc1ffd9af30e97c12c0c7fc2728adb8
Anibal-Medina <anibal.medina.mena@gmail.com>
2018-10-11 09:37:50 -0500

    Agrego el commit 3

--- proyectos/2/MedinaAnibal-MorenoGuadalupe/Monitor.py
+++ proyectos/2/MedinaAnibal-MorenoGuadalupe/Monitor.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from tkinter import *
+from tkinter import ttk
 import subprocess
 import time
 import sys
 import threading
+import psutil
 
 # Numero de nucleos que tiene nuestra computadora
 NumNucleos = int(subprocess.getoutput("grep processor /proc/cpuinfo | wc -l"))
@@ -66,3 +71,33 @@ def Sistema():
     cpu_uso = (int(estado2) - int(estado1)) / NumNucleos
     alertProces()
     return str(cpu_uso)
+
+# saber el porcentaje de uso que está Inactivo
+def cpuInactivo():
+    estado1 = subprocess.getoutput("cat /proc/stat | grep 'cpu ' | while read c1 c2 c3 c4 c5 c6; do echo $c5; done")
+    time.sleep(1)
+    estado2 = subprocess.getoutput("cat /proc/stat | grep 'cpu ' | while read c1 c2 c3 c4 c5 c6; do echo $c5; done")
+    cpu_uso = (int(estado2) - int(estado1)) / NumNucleos
+    alertProces()
+    return str(cpu_uso)
+
+
+
+# memoria total que tiene nuestra computadora, se muestra en kB
+def MemoTotal():
+    memtotal =  subprocess.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '1 p'")
+    alertProces()
+    return memtotal
+
+# memoria libre que tiene nuestra computadora( se muestra en kB)
+def MemoLibre():
+    memlibre =  subprocess.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '2 p'")
+    alertProces()
+    return memlibre
+
+# memoria que está usando el usuario(se muestra en kB)
+def MemoUso():
+    memuso =  subprocess.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '7 p'")
+    alertProcess()
+    return memuso
+
